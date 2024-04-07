9beebcd9a1284ed70fd5eabc660d810f52665303
Weijie Wang <wang.weijie0110@gmail.com>
2024-04-06 23:21:34 -0400

    jarvis at your service

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-import OpenAI from "openai";
-import dotenv from "dotenv";
-dotenv.config();
-
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+import openai from "./config/openai.js";
+import readlineSync from "readline-sync";
+import colors from "colors";
 
 async function main() {
-  const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "What is the capital of Canada?" }],
-  });
-  console.log(chatCompletion);
+  console.log(colors.bold.green("J.A.R.V.I.S. at your service!"));
+
+  const chatHistory = []; // store conversation history to save the context
+
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow("You: "));
+
+    try {
+      // construct messages by iterating over the history
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      // add latest user input
+      messages.push({ role: "user", content: userInput });
+
+      // call the api with user input
+      const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      // get completion text/content
+      const completionText = chatCompletion.choices[0].message;
+
+      if (
+        userInput.toLocaleLowerCase() === "jarvis off" ||
+        userInput.toLocaleLowerCase() === "off"
+      ) {
+        console.log(colors.green("J.A.R.V.I.S: As you wish."));
+        return;
+      }
+
+      console.log(colors.green("J.A.R.V.I.S: ") + completionText);
+
+      // update history with user input and assistant response
+      chatHistory.push(["user", userInput]);
+      chatHistory.push(["assistant", completionText]);
+    } catch (error) {
+      console.error(colors.red(error));
+    }
+  }
 }
 
 main();
